4feabf23c7109a2bac7f9edea36324fd29afbfde
ninoskopac <nino.skopac@gmail.com>
2018-05-01 19:34:10 +0700

    interfaces

---
+++ src/iTunesPodcastFeed/Interfaces/Episode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ninoskopac
+ * Date: 01/05/2018
+ * Time: 19:32
+ */
+declare(strict_types=1);
+namespace iTunesPodcastFeed;
+
+
+interface Episode
+{
+
+}
---
+++ src/iTunesPodcastFeed/Interfaces/FeedGenerator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ninoskopac
+ * Date: 01/05/2018
+ * Time: 19:33
+ */
+declare(strict_types=1);
+namespace iTunesPodcastFeed;
+
+interface FeedGenerator
+{
+
+}
