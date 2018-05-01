e366bc54567dc6f5ffcf235422393de9a89b1592
ninoskopac <nino.skopac@gmail.com>
2018-05-02 01:24:54 +0700

    usage example

--- example.php
+++ example.php
@@ -12,6 +12,7 @@ use iTunesPodcastFeed\Item;
 
 require __DIR__ . '/vendor/autoload.php';
 
+// SETUP CHANNEL
 $title = 'Read2Me Daily Curated Articles';
 $link = 'https://read2me.online';
 $author = 'NYTimes and Medium';
@@ -30,13 +31,13 @@ $lang = 'en';
 $copyright = 'The New York Times Company and The Medium Company';
 $ttl = 43200; // 12 hours in seconds
 
-// channel meta
 $channel = new Channel(
     $title, $link, $author, $email,
     $image, $explicit, $categories,
     $description, $lang, $copyright, $title
 );
 
+// SETUP EPISODE
 $title = "Trump Says Disclosure of Mueller Questions in Russia Probe Is ‘Disgraceful’";
 $file_Url = 'https://s3.read2me.online/audio/www-nytimes-com-2018-05-01-us-politics-trump-mueller-russia-questions-html-7e9601.mp3';
 $duration = '2:18';
@@ -45,13 +46,14 @@ $date = 1525177808;
 $filesize = 828387;
 $mime = 'audio/mpeg';
 
-// i.e. an episode
 $item = new Item(
     $title, $file_Url, $duration,
     $description, $date, $filesize, $mime
 );
 $item2 = clone $item; // just to give you an idea of how it works
 
+// SETUP FEED
 $feed = new FeedGenerator($channel, ...[$item, $item2]);
 
+// OUTPUT XML
 print $feed->getXml();
